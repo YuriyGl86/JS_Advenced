718a5e9adeaefa98c344a3e2284dcb38729ea139
YuriyGl86 <yuriygl86@gmail.com>
2023-07-23 22:33:04 +0300

    4 compl

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,6 +7,7 @@ import Undead from './characters/Undead';
 import Daemon from './characters/Daemon';
 import { generateTeam } from './generators';
 import PositionedCharacter from './PositionedCharacter';
+import GamePlay from './GamePlay';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -16,21 +17,43 @@ export default class GameController {
 
   init() {
     this.gamePlay.drawUi(themes.prairie);
-    this.placeTems();
+    this.placeTeams();
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this))
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this))
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this))
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
 
   onCellClick(index) {
     // TODO: react to click
+    const char = this.checkEmptyCell(index)
+    
+    if(char && ['bowman','swordsman','magician'].includes(char.character.type)){
+      console.log('position', char.position)
+      if(this.gamePlay.activeCell){
+        this.gamePlay.deselectCell(this.gamePlay.activeCell)
+      }      
+      this.gamePlay.selectCell(char.position)
+      this.gamePlay.activeCell = char.position
+    } else {
+      GamePlay.showError('it is not your character')
+    }
+
   }
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+    const char = this.checkEmptyCell(index)
+    if(char){
+      const msg = this.getTooltipMsg(char)
+      this.gamePlay.showCellTooltip(msg, index)
+    }
   }
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index)
   }
 
   getPositionNumber(player) {
@@ -53,20 +76,34 @@ export default class GameController {
     }
   }
 
-  placeTems() {
+  placeTeams() {
     const playerTeam = generateTeam([Bowman, Magician, Swordsman], 1, 4).toArray();
     const enemyTeam = generateTeam([Vampire, Undead, Daemon], 1, 4).toArray();
 
-    const positionedChars = [];
-    const cur = this;
+    this.gamePlay.positionedChars = [];
 
     playerTeam.forEach((char) => {
-      positionedChars.push(new PositionedCharacter(char, this.getPositionNumber('player')));
+      this.gamePlay.positionedChars.push(new PositionedCharacter(char, this.getPositionNumber('player')));
     });
     enemyTeam.forEach((char) => {
-      positionedChars.push(new PositionedCharacter(char, this.getPositionNumber('enemy')));
+      this.gamePlay.positionedChars.push(new PositionedCharacter(char, this.getPositionNumber('enemy')));
     });
 
-    this.gamePlay.redrawPositions(positionedChars);
+    this.gamePlay.redrawPositions(this.gamePlay.positionedChars);
+  }
+
+  checkEmptyCell(index){
+    let empt = false
+    this.gamePlay.positionedChars.forEach((PositionedCharacter) => {
+      if(PositionedCharacter.position === index){
+        empt = PositionedCharacter
+      }
+    })
+    return empt
+  }
+
+  getTooltipMsg(PositionedCharacter){
+    const char = PositionedCharacter.character
+    return `\u{1F396} ${char.level} \u{2694} ${char.attack} \u{1F6E1} ${char.defence} \u{2764} ${char.health}`
   }
 }
--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,4 +1,8 @@
 export default class GameState {
+  constructor(){
+    this.userTurn = true
+  }
+
   static from(object) {
     // TODO: create object
     return null;
